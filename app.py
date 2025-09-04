d8101be9b27e8e6b8bb1c882b525af7dba1117c3
Marco777457 <marcoht457@gmail.com>
2025-09-03 19:58:07 -0500

    Cambio puerto de code app.py

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def consultar_paciente(nombre):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # usa el puerto de Render o 5000 por defecto
+    app.run(host="0.0.0.0", port=port, debug=True)
