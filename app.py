860468666b4635cf62819546a025d7e38927ac7d
Marco777457 <marcoht457@gmail.com>
2025-09-05 17:53:17 -0500

    Cambio app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ def index():
 def registrar():
     nombre = request.form["nombre"]
     edad = request.form["edad"]
-    enfermedades = request.form["enfermedades"].split(",")
-    medicamentos = request.form["medicamentos"].split(",")
+    enfermedades = [e.strip() for e in request.form["enfermedades"].split(",") if e.strip()]
+    medicamentos = [m.strip() for m in request.form["medicamentos"].split(",") if m.strip()]
     contacto_nombre = request.form["contacto_nombre"]
     contacto_tel = request.form["contacto_tel"]
 
